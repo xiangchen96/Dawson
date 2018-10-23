60b384a3fac334c84667cc91cc004c0ff57c120c
Xiang <xiang1016127637@gmail.com>
2018-10-23 22:43:57 +0200

    Refactor into class

--- src/PvP.py
+++ src/PvP.py
@@ -1,66 +1,23 @@
 import pygame
 import time
-import dawson
+from dawson import Dawson
 
 pygame.init()
-
-WIDTH = 10
-screen = pygame.display.set_mode((90*WIDTH, 300))
-
-myfont = pygame.font.SysFont("monospace", 20)
+game = Dawson()
+pygame.display.flip()
 running = True
-turn = dawson.WHITE
-
-matrizPos = [[0]*WIDTH for _ in range(3)]
-matrizRect = [["PLACEHOLDER"]*WIDTH for _ in range(3)]
-
-dawson.reset(matrizPos)
-
-# draw board
-for i in range(3):
-    for j in range(WIDTH):
-        matrizRect[i][j] = pygame.Rect((90*j), (90*i), 90, 90)
-        if (i+j) % 2 == 0:
-            pygame.draw.rect(screen, dawson.dark_brown, matrizRect[i][j])
-        else:
-            pygame.draw.rect(screen, dawson.clear_brown, matrizRect[i][j])
-
-game_finished = False
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            if game_finished:
-                dawson.reset(matrizPos)
-                turn = dawson.WHITE
-                game_finished = False
-                # hide winner
-                pygame.draw.rect(screen, (0, 0, 0), pygame.Rect(0, 270, 900, 30))
+            if game.finished:
+                game.reset()
             else:
                 pos = pygame.mouse.get_pos()
-                for i in range(3):
-                    for j in range(WIDTH):
-                        if matrizRect[i][j].collidepoint(pos) and matrizPos[i][j] == turn:
-                            movs = dawson.possible_moves(matrizPos, turn)
-                            if j in [x for (x, _) in movs]:
-                                aux = 0
-                                for (a, b) in movs:
-                                    if a == j:
-                                        break
-                                    else:
-                                        aux += 1
-                                col, new_col = movs[aux]
-                                dawson.move(matrizPos, i, j, 1, new_col)
-                                turn = dawson.pass_turn(turn)
-                                if not dawson.possible_moves(matrizPos, turn):
-                                    game_finished = True
-                                    winner = "BLACK" if turn == dawson.WHITE else "WHITE"
-                                    label = myfont.render(winner+" WINS", 1, (255, 255, 0))
-                                    screen.blit(label, (390, 273))
-    moves_rect = dawson.rect_possible_moves(matrizPos, turn)
-    dawson.draw_state(screen, matrizPos, matrizRect, moves_rect)
-    pygame.display.flip()
+                game.process_click(pos)
+            game.draw_state()
+            pygame.display.flip()
 
 quit()
--- src/dawson.py
+++ src/dawson.py
@@ -14,81 +14,115 @@ WHITE = 1
 BLANK = 0
 
 
-def move(m, i0, j0, i, j):
-    color = m[i0][j0]
-    m[i0][j0] = BLANK
-    m[i][j] = color
+class Dawson:
+    def __init__(self):
+        width = int(input('Introduce the number of columns: '))
+        self.width = width
+        self.board_state = [[0]*width]*3
+        self.screen = pygame.display.set_mode((90*width, 300))
+        self.font = pygame.font.SysFont("monospace", 20)
+        self.rect_container = [[0]*self.width for _ in range(3)]
+        for i in range(3):
+            for j in range(width):
+                self.rect_container[i][j] = pygame.Rect((90*j), (90*i), 90, 90)
+        self.reset()
 
+    def reset(self):
+        self.board_state[0] = [BLACK]*self.width
+        self.board_state[1] = [BLANK]*self.width
+        self.board_state[2] = [WHITE]*self.width
+        self.turn = WHITE
+        self.finished = False
+        pygame.draw.rect(self.screen, (0, 0, 0), pygame.Rect(0, 270, 900, 30))
+        self.draw_state()
 
-def reset(m):
-    width = len(m[0])
-    m[0] = [BLACK]*width
-    m[1] = [BLANK]*width
-    m[2] = [WHITE]*width
+    def move(self, i, j, new_i, new_j):
+        color = self.board_state[i][j]
+        self.board_state[i][j] = BLANK
+        self.board_state[new_i][new_j] = color
 
+    def rect_possible_moves(self):
+        movs = self.possible_moves()
+        lista = []
+        if self.turn == WHITE:
+            for j in (x for (x, _) in movs):
+                lista.append((2, j))
+        else:
+            for j in (x for (x, _) in movs):
+                lista.append((0, j))
+        return lista
 
-def rect_possible_moves(m, turn):
-    movs = possible_moves(m, turn)
-    lista = []
-    if turn == WHITE:
-        for j in (x for (x, _) in movs):
-            lista.append((2, j))
-    else:
-        for j in (x for (x, _) in movs):
-            lista.append((0, j))
-    return lista
+    def pass_turn(self):
+        self.turn = BLACK if self.turn == WHITE else WHITE
 
+    def get_winner(self):
+        return "BLACK" if self.turn == WHITE else "WHITE"
 
-def pass_turn(turn):
-    if turn == BLACK:
-        return WHITE
-    else:
-        return BLACK
+    def possible_moves(self):
+        """
+        Returns a list with (origin_col, next_col)
+        """
+        movs = []
+        m = self.board_state
+        if self.turn == WHITE:
+            center_row_blacks = (j for j in range(self.width) if m[1][j] == BLACK)
+            # mandatory moves
+            for j in center_row_blacks:
+                if j-1 >= 0 and m[2][j-1] == WHITE:
+                    movs.append((j-1, j))
+                if j+1 < len(m[0]) and m[2][j+1] == WHITE:
+                    movs.append((j+1, j))
+            if not movs:
+                for j in range(self.width):
+                    if m[2][j] == WHITE and m[1][j] == BLANK:
+                        movs.append((j, j))
+        else:
+            center_row_whites = (j for j in range(self.width) if m[1][j] == WHITE)
+            for j in center_row_whites:
+                if j-1 >= 0 and m[0][j-1] == BLACK:
+                    movs.append((j-1, j))
+                if j+1 < len(m[0]) and m[0][j+1] == BLACK:
+                    movs.append((j+1, j))
+            if not movs:
+                for j in range(self.width):
+                    if m[0][j] == BLACK and m[1][j] == BLANK:
+                        movs.append((j, j))
+        return movs
 
+    def draw_state(self):
+        for i in range(3):
+            for j in range(self.width):
+                if (i+j) % 2 == 0:
+                    pygame.draw.rect(self.screen, dark_brown, self.rect_container[i][j])
+                else:
+                    pygame.draw.rect(self.screen, clear_brown, self.rect_container[i][j])
+                if (i, j) in self.rect_possible_moves():
+                    self.screen.blit(pawn_contour, (90*j, 90*i))
+                # pawns
+                if self.board_state[i][j] == BLACK:
+                    self.screen.blit(black_pawn, (1+90*j, 90*i))
+                elif self.board_state[i][j] == WHITE:
+                    self.screen.blit(white_pawn, (1+90*j, 90*i))
 
-def draw_state(screen, matrizPos, matrizRect, moves_rect):
-    for i in range(3):
-        for j in range(len(matrizPos[0])):
-            if (i, j) in moves_rect:
-                screen.blit(pawn_contour, (90*j, 90*i))
-            elif (i+j) % 2 == 0:
-                pygame.draw.rect(screen, dark_brown, matrizRect[i][j])
-            else:
-                pygame.draw.rect(screen, clear_brown, matrizRect[i][j])
-            # pawns
-            if matrizPos[i][j] == BLACK:
-                screen.blit(black_pawn, (1+90*j, 90*i))
-            elif matrizPos[i][j] == WHITE:
-                screen.blit(white_pawn, (1+90*j, 90*i))
+    def draw_winner_label(self):
+        label = self.font.render(self.get_winner()+" WINS", 1, (255, 255, 0))
+        self.screen.blit(label, (90*self.width/2, 273))
 
-
-def possible_moves(m, turn):
-    """
-    Returns a list with (col_ini, col_dest)
-    """
-    movs = []
-    if turn == WHITE:
-        middle_black_list = filter(lambda j: m[1][j] == BLACK, range(len(m[0])))
-        # mandatory moves
-        for j in middle_black_list:
-            if j-1 >= 0 and m[2][j-1] == WHITE:
-                movs.append((j-1, j))
-            if j+1 < len(m[0]) and m[2][j+1] == WHITE:
-                movs.append((j+1, j))
-        if not movs:
-            for j in range(len(m[0])):
-                if m[2][j] == turn and m[1][j] == BLANK:
-                    movs.append((j, j))
-    else:
-        listaColBlancoEnMedio = filter(lambda j: m[1][j] == WHITE, range(len(m[0])))
-        for j in listaColBlancoEnMedio:
-            if j-1 >= 0 and m[0][j-1] == BLACK:
-                movs.append((j-1, j))
-            if j+1 < len(m[0]) and m[0][j+1] == BLACK:
-                movs.append((j+1, j))
-        # no hay obligatorios
-        if not movs:
-            for j in range(len(m[0])):
-                if m[0][j] == BLACK and m[1][j] == BLANK:
-                    movs.append((j, j))
-    return movs
+    def process_click(self, pos):
+        for i in range(3):
+            for j in range(self.width):
+                if self.rect_container[i][j].collidepoint(pos) and self.board_state[i][j] == self.turn:
+                    movs = self.possible_moves()
+                    if j in [x for (x, _) in movs]:
+                        aux = 0
+                        for (a, b) in movs:
+                            if a == j:
+                                break
+                            else:
+                                aux += 1
+                        col, new_col = movs[aux]
+                        self.move(i, j, 1, new_col)
+                        self.pass_turn()
+                        if not self.possible_moves():
+                            self.finished = True
+                            self.draw_winner_label()
