7510539e7ec8058c28fd0ff0e76f2658bf03eeb4
Xiang <xiang1016127637@gmail.com>
2018-10-27 14:18:17 +0200

    Cleanup

--- src/dawson.py
+++ src/dawson.py
@@ -1,5 +1,7 @@
 import os
 import pygame
+import time
+import random
 from functools import lru_cache
 
 here = os.path.dirname(os.path.abspath(__file__))
@@ -16,7 +18,7 @@ BLANK = 0
 
 
 class Dawson:
-    def __init__(self, width):
+    def __init__(self, width=5):
         self.width = width
         self.board_state = [[0]*width]*3
         self.screen = pygame.display.set_mode((90*width, 300))
@@ -34,13 +36,14 @@ class Dawson:
         self.turn = WHITE
         self.finished = False
         # hide winner
-        pygame.draw.rect(self.screen, (0, 0, 0), pygame.Rect(0, 270, 90*self.width, 30))
+        self.screen.fill((0, 0, 0))
         self.draw_state()
 
     def move(self, i, j, new_i, new_j):
         color = self.board_state[i][j]
         self.board_state[i][j] = BLANK
         self.board_state[new_i][new_j] = color
+        self.pass_turn()
 
     def rect_possible_moves(self):
         movs = self.possible_moves()
@@ -99,17 +102,17 @@ class Dawson:
             return
         for i in range(3):
             for j in range(self.width):
-                if self.rect_container[i][j].collidepoint(pos) and self.board_state[i][j] == self.turn:
-                    movs = self.possible_moves()
-                    for (col, new_col) in (mov for mov in movs if mov[0] == j):
-                        self.move(i, j, 1, new_col)
-                        self.pass_turn()
-                        self.check_winner()
-                    self.draw_state()
+                if self.rect_container[i][j].collidepoint(pos):
+                    if self.board_state[i][j] == self.turn:
+                        movs = self.possible_moves()
+                        for (col, new_col) in (mov for mov in movs if mov[0] == j):
+                            self.move(i, j, 1, new_col)
+                            self.check_winner()
+                            self.draw_state()
                     return
 
 
-def digital_sum(L):
+def reduce_with_xor(L):
     suma = 0
     for i in L:
         suma ^= i
@@ -117,6 +120,7 @@ def digital_sum(L):
 
 
 def mex(L):
+    """Return the minimum excluded value of a list"""
     L = set(L)
     n = 0
     while n in L:
@@ -124,81 +128,40 @@ def mex(L):
     return n
 
 
-def sgDawson(L):
-    return digital_sum((gDawson(i) for i in L))
-
-
-def to_binary(decimal):
-    return list(int(i) for i in bin(decimal)[2:])
-
-
 def successors(n):
-    if n < 1:
+    if n == 0:
         return []
     if n == 1:
-        return [[0]]
-    lista = []
-    lista.append([n-2])
+        return [(0,)]
+    successors_list = [(n-2,)]
     if n >= 3:
-        lista.append([n-3])
+        successors_list.append((n-3,))
         j = n-3
         for i in range(1, int((j/2))+1):
-            lista.append([i, j-i])
-    return lista
+            successors_list.append((i, j-i))
+    return successors_list
 
 
 @lru_cache(maxsize=None)
 def gDawson(n):
+    """Return the G-value of a single Dawson game"""
     if n == 0:
         return 0
     if n == 1:
         return 1
-    listaMex = []
-    for i in successors(n):
-        if len(i) == 1:
-            listaMex.append(gDawson(i[0]))
+    successors_g = []
+    for next_state in successors(n):
+        if len(next_state) == 1:
+            successors_g.append(gDawson(next_state[0]))
         else:
-            listaMex.append(gDawson(i[0]) ^ gDawson(i[1]))
-    return mex(listaMex)
+            successors_g.append(gDawson(next_state[0]) ^ gDawson(next_state[1]))
+    return mex(successors_g)
 
 
-def apnim(posN, i=0):
-    suma = digital_sum(posN)
-    if suma:
-        bina, found = len(to_binary(suma)), False
-        while not found:
-            sumando = to_binary(posN[i])
-            if len(sumando) < bina or sumando[-bina] == 0:
-                i += 1
-            else:
-                found = True
-        return digital_sum([suma, posN[i]]), i
-    else:
-        return
+def sgDawson(L):
+    """Return the G-value of the sum of multiple Dawson games"""
+    return reduce_with_xor((gDawson(i) for i in L))
 
 
-def calcMovDawson(L):
-    # CASOS BASE
-    if sgDawson(L) == 0:
-        return "Es P"
-    if len(L) == 1 and L[0] == 1:
-        return []
-    result = L[:]
-    valPilas = list(map(gDawson, L))
-    gBuscado, indpilaCambiar = apnim(valPilas)
-    pila = L[indpilaCambiar]
-    if pila >= 2 and gDawson(pila-2) == gBuscado:
-        result[indpilaCambiar] = pila-2
-        result = [a for a in result if a != 0]
-        return result
-    for i in successors(pila):
-        if len(i) == 1 and sgDawson(i) == gBuscado:
-            result[indpilaCambiar] = i[0]
-            result = [a for a in result if a != 0]
-            return result
-        else:
-            if sgDawson(i) == gBuscado:
-                result[indpilaCambiar] = i[0]
-                result.insert(indpilaCambiar+1, i[1])
-                result = [a for a in result if a != 0]
-                return result
+def to_binary(decimal):
+    return [int(i) for i in bin(decimal)[2:]]
